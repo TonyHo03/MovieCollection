d86e24db4d54d9fa37092a066f5fae9834381bb7
tobiasiversenn <tobive01@easv.dk>
2026-01-06 09:29:15 +0100

    GUI add category and movie

--- src/main/java/dk/easv/moviecollection/HelloController.java
+++ src/main/java/dk/easv/moviecollection/HelloController.java
@@ -1,7 +1,12 @@
 package dk.easv.moviecollection;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class HelloController {
     @FXML
@@ -11,4 +16,43 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
-}
+
+    public void btnOnNewMovie(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/dk/easv/moviecollection/addMovieView.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage stage = new Stage();
+            stage.setTitle("New Movie");
+
+            stage.initModality(Modality.APPLICATION_MODAL);
+
+
+
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (Exception e) {
+
+        }
+    }
+
+    public void btnOnNewCategory(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/dk/easv/moviecollection/addCategory.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage stage = new Stage();
+            stage.setTitle("New category");
+
+            stage.initModality(Modality.APPLICATION_MODAL);
+
+
+
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (Exception e) {
+
+        }
+    }
+    }
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module dk.easv.moviecollection {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens dk.easv.moviecollection to javafx.fxml;
