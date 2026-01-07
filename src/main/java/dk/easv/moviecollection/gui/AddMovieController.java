c2058589aaf1564655d038d04053b32c337bd003
tobiasiversenn <tobive01@easv.dk>
2026-01-07 10:08:33 +0100

    createMovie

--- src/main/java/dk/easv/moviecollection/gui/AddMovieController.java
+++ src/main/java/dk/easv/moviecollection/gui/AddMovieController.java
@@ -1,18 +1,25 @@
 package dk.easv.moviecollection.gui;
 
+import dk.easv.moviecollection.dal.db.MovieCollectionDAO_DB;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import dk.easv.moviecollection.BE.Movie;
 
 public class AddMovieController
 {
 
+    public MovieCollectionDAO_DB MovieDAO;
     @FXML
     public TextField txtMovieTitle;
     public TextField txtMovieCategory;
+    public TextField txtFilePath;
+    public TextField txtDate;
+    public TextField txtRating;
     public void btnAddMovie(ActionEvent actionEvent) {
 
-        String title = txtMovieTitle.getText();
-        String category = txtMovieCategory.getText();
+        Movie newMovie = new Movie(txtMovieTitle.getText(), txtMovieCategory.getText(), txtFilePath.getText(), txtDate.getText(), txtRating.getText());
+
+
     }
 }
